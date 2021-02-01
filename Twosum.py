e84645f41929ba6e0b54799a74fc5cdd555852a8
DevinHale1 <46941199+DevinHale1@users.noreply.github.com>
2021-02-01 13:57:47 -0500

    Create Twosum.py
    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
    
    You may assume that each input would have exactly one solution, and you may not use the same element twice.
    
    You can return the answer in any order.

---
+++ Twosum.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        target = 9
+        test = 0
+        nums = [2,7,11,15]
+        count = 0
+        count1 = 1
+        solution = []
+        while test != target:
+        
+            test = nums[count] + nums[count1]
+            if test == target:
+                solution.append(count)
+                solution.append(count1)
+                return solution
+
+            else:
+                count1 +=1
+                               
+            if count1 > len(nums) - 1:
+                count +=1
+                count1 = 0
+                
+            if count1 and count > len(nums) -1:
+                return "No solution"
